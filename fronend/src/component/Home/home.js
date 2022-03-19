5736779375df2dd6df495ceeeed26f8f33e7dacc
nghiepnguyenn277 <nghiepnguyenn277@gmail.com>
2022-03-19 20:10:35 +0700

    css code Product page

--- fronend/src/component/Home/home.js
+++ fronend/src/component/Home/home.js
@@ -1,8 +1,15 @@
 import React, { Fragment } from 'react'
 import { CgMouse } from 'react-icons/cg';
 import "./home.css"
+import Product from "./product.js"
 
-
+const product ={
+  name:"Shoes1",
+  images:[{url:"https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_2.0/h_308,c_limit/92947e6d-a7d9-4b6a-8020-040f0cc61879/air-max-2021-shoes-MljWq1.png"}],
+  price:"200$",
+  _id:"sample",
+    size:"26"
+};
 
  const home = () => {
   return (
@@ -11,11 +18,15 @@ import "./home.css"
                 <p> Welcome to Nin store</p>
             <a href="#container">
                  <button>
-                Scroll <CgMouse/>
+                Scroll mouse <CgMouse/>
                 </button>
             </a>
         </div>
         <h2 className="HomeHeading">Featureed Products</h2>
+        <div className='container' id='container'>
+            <Product product={product}/>
+
+        </div>
         
     </Fragment>
   );
