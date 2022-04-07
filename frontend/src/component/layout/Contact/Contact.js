a4d06fb605feeffed5d1581465868a8e145a68ec
nghiepnguyenn277 <nghiepnguyenn277@gmail.com>
2022-04-07 10:34:13 +0700

    debug

--- frontend/src/component/layout/Contact/Contact.js
+++ frontend/src/component/layout/Contact/Contact.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./Contact.css";
-import { Button } from "@material-ui/core";
+
 
 const Contact = () => {
   return (
     <div className="contactContainer">
-      <a className="mailBtn" href="mailto:mymailforabhi@gmail.com">
-        <Button>Contact: mymailforabhi@gmail.com</Button>
+      <a>
+        <h1>Contact: nghiepnlgcc18037@gmail@gmail.com</h1>
       </a>
     </div>
   );
